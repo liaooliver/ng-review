244a48e9b01fcc61ade0cc210cd8b707c17bae4c
Oliver Liao <rtw0913@gmail.com>
2021-08-04 14:40:52 +0800

    update v10 -> v11

--- src/app/platform/profile/profile.component.spec.ts
+++ src/app/platform/profile/profile.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProfileComponent ]
     })
